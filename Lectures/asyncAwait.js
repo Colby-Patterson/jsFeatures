ec562fb9dc9745cc8238bf42a4bd958b912ed5ad
Colby Patterson <cpatt@byui.edu>
2022-05-25 10:01:49 -0600

    Async Await

--- Lectures/asyncAwait.js
+++ Lectures/asyncAwait.js
@@ -1,5 +1,24 @@
-const f = ()=>{
-  return new Promise((resolve, reject)=>{
+const fakeAPICall = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(url === 'goodurl'){
+            resolve({data:['joe','john']})
+        } else {
+            reject({error:'404 page not found'})
+        }
+    }, 20000);
+  });
+};
 
-  })
+// async await function
+const getData = async () => {
+  // fakeAPICall returns a promise i use the await key word
+  console.log('before api call')
+  let response = await fakeAPICall('url')
+  console.log('after api call')
+  console.log('response:', response)
 }
+
+console.log('before getData call') // 1
+getData()
+console.log('after getData call')
