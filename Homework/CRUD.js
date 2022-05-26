a1026fc5c0b29ee19baf243da01fb07d3cc89cc9
Colby Patterson <cpatt@byui.edu>
2022-05-26 10:33:01 -0600

    Create, read, update, delete

---
+++ Homework/CRUD.js
@@ -0,0 +1,30 @@
+// Create(POST), Read(GET), Update(PUT), Delete (web 101)
+
+// CRUD in js
+
+// READ
+let todos = [{id: 1, complete: false, name: 'eat'},
+{id: 2, complete: false, name: 'sleep'},
+{id: 3, complete: false, name: 'code'}
+];
+
+//READ
+const getInitTodos = ()=>{
+  return todos
+}
+
+const addTodo = (listOfTodos, todo)=>{
+  return [...listOfTodos, todo]
+}
+
+const updateTodo = (listOfTodos, id)=>{
+  // map
+  return listOfTodos.map((t)=>t.id === id ? {...t, complete: !t.complete} : t)
+//   return listOfTodos.map(t=>{
+//     if(t.id === id){
+//       return {...t, complete: !t.complete}
+//     }
+//     return t
+//   })
+}
+module.exports = { getInitTodos, addTodo, updateTodo }
---
+++ test/CRUD.test.js
@@ -0,0 +1,29 @@
+const {getInitTodos, addTodo, updateTodo} = require('../Homework/CRUD');
+
+it('test Read', ()=>{
+    expect(getInitTodos().length).toBe(3)
+})
+
+it('test Create', ()=>{
+  //Go to a website and grab the todos
+  let todos = getInitTodos()
+  // Then we go to a form and create a new todo
+  const newTodo = {id: 3, complete: false, name: 'dishes'}
+  // We can't mutate the todos
+  expect(addTodo(todos, newTodo).length).toBe(4)
+  expect(todos.length).toBe(3)
+})
+
+it('test Update', ()=>{
+  //Go to a website and grab the todos
+  let todos = getInitTodos()
+  // Then we go to a form and create a new todo
+  
+  // update todo should return a new list of todos with the item
+  //complete toggled
+  let newTodos = updateTodo(todos, 1)
+  let newFirstTodo = newTodos[0]
+  let oldFirstTodo = todos[0]
+  expect(newFirstTodo.complete).toBe(true)
+  expect(oldFirstTodo.complete).toBe(false)
+})
